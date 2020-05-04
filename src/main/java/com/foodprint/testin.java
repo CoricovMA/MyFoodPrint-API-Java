f6ee4d37e00a167a5114be7e98ff4b8d13405109
CoricovMA <coricovacMA@gmail.com>
2020-05-04 19:03:32 -0400

    found tagger, playing around with it

--- src/main/java/com/foodprint/testin.java
+++ src/main/java/com/foodprint/testin.java
@@ -1,22 +1,44 @@
 package com.foodprint;
 
+import com.foodprint.database.Database;
 import com.foodprint.response.FoodPrintRequest;
 import com.foodprint.response.FoodPrintResponse;
+import com.foodprint.util.StringParser;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.ling.SentenceUtils;
+import edu.stanford.nlp.ling.TaggedWord;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.POSTaggerAnnotator;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.process.CoreLabelTokenFactory;
+import edu.stanford.nlp.process.PTBTokenizer;
+import edu.stanford.nlp.process.TokenizerFactory;
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class testin {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
         Logger logger = LogManager.getLogger(testin.class);
-
+        long startTime = System.currentTimeMillis();
         logger.info("Hello");
-
-        String param = "1 kg beef, 1 kg chicken";
-        FoodPrintRequest foodPrintRequest = new FoodPrintRequest(param);
-        System.out.println(foodPrintRequest);
-        FoodPrintResponse foodPrintResponse = new FoodPrintResponse().setRequest(foodPrintRequest);
-        System.out.println(foodPrintResponse);
+        String ingredientString = "1kg chopped beef";
+        MaxentTagger tagger = new MaxentTagger();
+        tagger.tagString("hello");
+//        String taggedString = StringParser.getTagger().tagString(ingredientString);
+//        System.out.println(taggedString);
+//        System.out.println(StringParser.getInstance().getUnitsFromString(ingredientString));
+//        System.out.println(StringParser.getInstance().getVolumeFromString(ingredientString));
+//        System.out.println(System.currentTimeMillis()-startTime + "ms");
     }
 }
--- src/main/java/com/foodprint/util/StringParser.java
+++ src/main/java/com/foodprint/util/StringParser.java
@@ -1,5 +1,6 @@
 package com.foodprint.util;
 
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.languagetool.JLanguageTool;
@@ -7,7 +8,13 @@ import org.languagetool.language.AmericanEnglish;
 import org.languagetool.language.BritishEnglish;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import edu.stanford.nlp.tagger.*;
 
 public class StringParser {
 
@@ -16,10 +23,13 @@ public class StringParser {
         BRITISH_ENGLISH
     }
 
+    private static Map<String, Double> volumes = new HashMap<>();
+
     private static Logger logger = LogManager.getLogger(StringParser.class);
     private static StringParser instance;
     private static JLanguageTool languageTool;
     private static LANGUAGE currentLanguage;
+    private static MaxentTagger tagger = new MaxentTagger();
 
     private StringParser() {
         languageTool = new JLanguageTool(new AmericanEnglish());
@@ -31,6 +41,24 @@ public class StringParser {
         } catch (IOException e) {
             logger.info("Error doing first check. ", e);
         }
+
+        volumes.put("ml", 0.001);
+        volumes.put("g", 0.001);
+        volumes.put("grams", 0.001);
+        volumes.put("tsp", 0.005);
+        volumes.put("tbsp", 0.015);
+        volumes.put("oz", 0.028);
+        volumes.put("cup", 0.23658);
+        volumes.put("cups", 0.236588);
+        volumes.put("kg", 1.0);
+        volumes.put("lb", 0.453592);
+        volumes.put("pinch", 0.00036);
+        volumes.put("kilogram", 1.0);
+        volumes.put("kilograms", 1.0);
+        volumes.put("pound", 0.453592);
+        volumes.put("pounds", 0.453592);
+        volumes.put("cloves", 0.013);
+        volumes.put("clove", 0.013);
     }
 
     public void setLanguage(LANGUAGE desiredLanguage) {
@@ -56,7 +84,11 @@ public class StringParser {
         this._setLanguage(desiredLanguage);
     }
 
-    private void _setLanguage(LANGUAGE desiredLanguage){
+    public static MaxentTagger getTagger(){
+        return tagger;
+    }
+
+    private void _setLanguage(LANGUAGE desiredLanguage) {
         currentLanguage = desiredLanguage;
     }
 
@@ -70,6 +102,7 @@ public class StringParser {
     /**
      * Checks a given string for spelling errors, and returns
      * suggested replacements in case there are errors
+     *
      * @param givenString String to check for spelling errors
      * @return list of possible replacements
      */
@@ -84,5 +117,34 @@ public class StringParser {
         return matches;
     }
 
+    public String getUnitsFromString(String givenIngredientString) {
+        Pattern p = Pattern.compile("\\d+");
+        Matcher m = p.matcher("1 kg chicken");
+        if (m.find())
+            return m.group();
+        else
+            return null;
+    }
+
+    public String getVolumeFromString(String givenIngredientString) {
+        String[] stringAsArray = removeIntegersFromString(givenIngredientString).split(" ");
+        String volumeFromString = null;
+        for(String word: stringAsArray){
+            if (volumes.containsKey(word)){
+                volumeFromString = word;
+                break;
+            }
+        }
+
+        return volumeFromString;
+    }
+
+    public static double getQuantityFromVol(String givenVol){
+        return volumes.get(givenVol);
+    }
+
+    public String removeIntegersFromString(String givenString){
+        return givenString.replaceAll("\\d", "");
+    }
 
 }
