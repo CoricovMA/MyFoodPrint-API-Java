bf4a8d6742a14b37adcf59eb7c73d6c7eb273cb7
CoricovMA <coricovacMA@gmail.com>
2020-05-04 22:01:26 -0400

    found openNLP for pos tagging

--- src/main/java/com/foodprint/testin.java
+++ src/main/java/com/foodprint/testin.java
@@ -1,29 +1,8 @@
 package com.foodprint;
 
-import com.foodprint.database.Database;
-import com.foodprint.response.FoodPrintRequest;
-import com.foodprint.response.FoodPrintResponse;
-import com.foodprint.util.StringParser;
-import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.ling.HasWord;
-import edu.stanford.nlp.ling.SentenceUtils;
-import edu.stanford.nlp.ling.TaggedWord;
-import edu.stanford.nlp.pipeline.Annotation;
-import edu.stanford.nlp.pipeline.POSTaggerAnnotator;
-import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.process.CoreLabelTokenFactory;
-import edu.stanford.nlp.process.PTBTokenizer;
-import edu.stanford.nlp.process.TokenizerFactory;
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class testin {
 
@@ -33,7 +12,7 @@ public class testin {
         long startTime = System.currentTimeMillis();
         logger.info("Hello");
         String ingredientString = "1kg chopped beef";
-        MaxentTagger tagger = new MaxentTagger();
+        MaxentTagger tagger = new MaxentTagger("C:\\Users\\AMC\\IdeaProjects\\MyFoodPrint-API-Java\\src\\main\\java\\com\\foodprint\\tagger\\models\\bidirectional-distsim-wsj-0-18.tagger");
         tagger.tagString("hello");
 //        String taggedString = StringParser.getTagger().tagString(ingredientString);
 //        System.out.println(taggedString);
